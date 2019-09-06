6a8595716ef538e7643f38f2f63b10ba43db7a35
RainGather <raingather@outlook.com>
2019-09-06 19:01:08 +0800

    [UPDATE] tutorial

--- tutorial/mgr/__init__.py
+++ tutorial/mgr/__init__.py
@@ -15,8 +15,18 @@ auth_path = pathlib.Path(__file__).parent / 'auth.json'
 class Mgr:
     def __init__(self):
         self.HOST = 'http://www.hzasteam.org:8097'
+        self.DEBUG_HOST = 'http://localhost:5000'
         self.AUTH_PATH = pathlib.Path(__file__).parent / 'auth.json'
-        self.WECHAT = 'Assert_'
+        self.CONTACK = '微信：Assert_，QQ群：820510014(申请备注:EasyPython)'
+        self.check_debug()
+    
+    def check_debug(self):
+        try:
+            r = requests.get(f'{self.DEBUG_HOST}/username_not_exists/test', timeout=0.5)
+            if r.ok or r.status_code == 401:
+                self.HOST = self.DEBUG_HOST
+        except Exception as e:
+            pass
     
     def save_auth_info(self, username, pwd):
         mac = self.get_mac_address()
@@ -117,7 +127,7 @@ class Mgr:
         elif r.status_code == 401:
             print('用户认证错误！')
         elif r.status_code == 403:
-            print(f'账户试用期已到，请联系微信{self.WECHAT}')
+            print(f'账户试用期已到，请联系{self.CONTACK}')
         else:
             print('网络错误！请检查网络或换个时间重试。')
         os.remove(py_path)
@@ -137,6 +147,8 @@ class Mgr:
             elif r.status_code == 403:
                 print(f'账户试用期已到，请联系微信{self.WECHAT}')
         else:
+            if self.HOST == self.DEBUG_HOST:
+                print('DEBUG MODE')
             url = f'{api}/test/{index}'
             r = requests.post(url, json={'auth': self.get_auth()})
             if r.ok:
@@ -197,32 +209,3 @@ sub = mgr.sub
 get = mgr.get
 reg = mgr.reg
 login = mgr.login
-
-# def set_name():
-#     username = ''
-#     while len(username) < 2:
-#         username = input('请输入你的昵称，至少2位（尽量长一些，不要和别人重名）：').strip()
-#     with auth_path.open('w', encoding='utf-8') as fw:
-#         json.dump({'username': username}, fw)
-
-# def set_auth():
-#     print('你的试用已结束，请完成如下信息填写：')
-#     with auth_path.open('r', encoding='utf-8') as fr:
-#         username = json.load(fr)['username']
-#     mac = get_mac_address()
-#     id_ = hashlib.sha256(f'{username}-{mac}'.encode('utf-8')).hexdigest()[:10]
-#     print(f'你的ID为：{id_}')
-#     print('你可以加微信xxx，发送你的ID给对方，获取序列号')
-#     print('如已购买课程是免费获取的，如果你是在不同电脑上使用跳出此提示，也请联系该微信号')
-#     token = input('请输入你获取到的序列号：')
-#     auth = {
-#         'username': username,
-#         'id': id_,
-#         'token': token
-#     }
-#     r = requests.post(f'{HOST}/auth_check', json=auth)
-#     if r.ok and r.json().get('state'):
-#         with auth_path.open('w', encoding='utf-8') as fw:
-#             json.dump(auth, fw)
-#         return True
-#     return False
