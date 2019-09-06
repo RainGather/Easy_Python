7984d917ec35f7c27ca5c926a27afaee5fe04652
RainGather <raingather@outlook.com>
2019-09-06 12:24:12 +0800

    [bugfix] start git pull stash

--- start.bat
+++ start.bat
@@ -4,5 +4,7 @@ echo 系统更新中...
 cd %~dp0\tutorial
 C:\git\bin\git.exe stash
 C:\git\bin\git.exe pull origin master
+C:\git\bin\git.exe stash pop
+C:\git\bin\git.exe stash clear
 echo 系统启动中...
 C:\python3\Scripts\jupyter-lab
