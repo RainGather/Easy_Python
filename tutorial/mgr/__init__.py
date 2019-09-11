24fd75707281bf46c784202e6d82ed4f171a39cd
RainGather <raingather@outlook.com>
2019-09-11 14:33:21 +0800

    [bugfix] toturial

--- tutorial/mgr/__init__.py
+++ tutorial/mgr/__init__.py
@@ -145,7 +145,7 @@ class Mgr:
             elif r.status_code == 401:
                 print('用户认证错误！')
             elif r.status_code == 403:
-                print(f'账户试用期已到，请联系微信{self.WECHAT}')
+                print(f'账户试用期已到，请联系微信{self.CONTACK}')
             else:
                 print('网络错误！')
         else:
