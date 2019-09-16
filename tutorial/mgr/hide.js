f0df70acb7c10d261fec7fade7f3256aa82f30e8
RainGather <raingather@outlook.com>
2019-09-16 09:21:12 +0800

    [update] v1.0

---
+++ tutorial/mgr/hide.js
@@ -0,0 +1,57 @@
+function hide_input() {
+    var cell_i = 0
+    var divs = document.getElementsByTagName("div")
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].getAttribute("class") == 'input' && (divs[i].textContent.indexOf("from mgr import") != -1 || divs[i].textContent.indexOf("%%html") != -1 || divs[i].textContent.indexOf("%%js") != -1)) { 
+            if (divs[i].textContent.indexOf("%%js") == -1) {
+                Jupyter.notebook.execute_cells([cell_i])
+            }
+            divs[i].style.display = 'none'
+        }
+        if (divs[i].getAttribute("class") && (divs[i].getAttribute("class").indexOf('cell text_cell') != -1 || divs[i].getAttribute("class").indexOf('cell code_cell') != -1)) {
+            cell_i ++
+        }
+    }
+}
+
+function if_login_btn_show_cb(out_data){
+    var output = out_data.content.text.trim()
+    if (output != "False") {
+        $("#username_show").text(output)
+        $("#username_show").show()
+        $("#login_btn").hide()
+        $(".login_ui").hide()
+        $(".reg_ui").hide()
+        $(".change_pwd_ui").hide()
+        $("#logout_btn").show()
+        $("#manage_btn").show()
+        $("#reg_btn").hide()
+    } else {
+        $("#username_show").hide()
+        $("#login_btn").show()
+        $("#logout_btn").hide()
+        $("#manage_btn").hide()
+        $("#reg_btn").show()
+    }
+}
+
+function if_login_btn_show() {
+    var callback = {
+            iopub : {
+                output : if_login_btn_show_cb,
+        }
+    }
+    var code = "import os, json\np=os.path.join('mgr', 'auth.json')\nif os.path.exists(p):\n with open(p, 'r', encoding='utf-8') as fr:\n  c=json.load(fr)\n print(c['username'])\nelse:\n print(False)"
+    IPython.notebook.kernel.execute(code, callback)
+}
+
+function login_show() {
+    var login = document.getElementsByTagName("login")[0]
+    if (login.style.display == "none"){
+        login.style.display = "inline"
+    } else {
+        login.style.display = "none"
+    }
+}
+
+IPython.notebook.save_notebook()
---
+++ tutorial/mgr/submit.js
@@ -0,0 +1,159 @@
+var FLAG = "退出账号</button>"
+
+function get_filename() {
+    var divs = document.getElementsByTagName("span")
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].getAttribute("class") == 'filename') {
+            return divs[i].textContent
+        }
+    }
+    return false
+}
+
+function get_source_code() {
+    var cell_i = 0
+    var divs = document.getElementsByTagName("div")
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].getAttribute("class") && (divs[i].getAttribute("class").indexOf("cell text_cell") != -1 || divs[i].getAttribute("class").indexOf("cell code_cell") != -1)) {
+            if (divs[i].getAttribute("class").indexOf("cell code_cell") != -1 && divs[i].textContent.indexOf(FLAG) != -1) {
+                var code = Jupyter.notebook.get_cell(cell_i - 1)
+                if (code) {
+                    return code.get_text()
+                } else {
+                    return false
+                }
+            }
+            cell_i++
+        }
+    }
+    return false
+}
+
+function output_(out_data) {
+    if (out_data.msg_type == "error") {
+        result = ""
+        for (var error in out_data.content.traceback) {
+            result += out_data.content.traceback[error].replace(/\n/g, "<br>").trim() + "<br>"
+        }
+        var output = result.replace(/\[.*?m/g, '') + out_data.content.evalue.replace(/\n/g, "<br>")
+        document.getElementsByTagName("output")[0].innerHTML = output
+    } else {
+        var output = out_data.content.text.replace(/\n/g, "<br>")
+        document.getElementsByTagName("output")[0].innerHTML = output
+    }
+}
+
+function change_pwd() {
+    var old_pwd = $("#old_pwd").val()
+    var new_pwd = $("#new_pwd").val()
+    var chk_new_pwd = $("#chk_new_pwd").val()
+    $("#old_pwd").val("")
+    $("#new_pwd").val("")
+    $("#chk_new_pwd").val("")
+    if (chk_new_pwd != new_pwd) {
+        requirejs(['mdui'], function(mdui) {
+            mdui.alert("密码不一致！")
+        })
+        return false
+    }
+    change_pwd_code = "import mgr\nmgr.change_pwd('" + old_pwd + "', '" + new_pwd + "')"
+    var callback = {
+        iopub: {
+            output: notice,
+        }
+    }
+    IPython.notebook.kernel.execute(change_pwd_code, callback)
+}
+
+function reg() {
+    var username = $("#reg_username").val()
+    var pwd = $("#reg_pwd").val()
+    var chk_pwd = $("#reg_chk_pwd").val()
+    if (chk_pwd != pwd) {
+        requirejs(['mdui'], function(mdui) {
+            mdui.alert("密码不一致！")
+        })
+        return false
+    }
+    reg_code = "import mgr\nmgr.reg('" + username + "', '" + pwd + "')"
+    var callback = {
+        iopub: {
+            output: notice,
+        }
+    }
+    IPython.notebook.kernel.execute(reg_code, callback)
+}
+
+function notice(out_data) {
+    if (out_data.msg_type == "error") {
+        result = ""
+        for (var error in out_data.content.traceback) {
+            result += out_data.content.traceback[error].replace(/\n/g, "<br>").trim() + "<br>"
+        }
+        var output = result.replace(/\[.*?m/g, '') + out_data.content.evalue.replace(/\n/g, "<br>")
+        $("#output").html(output)
+    } else {
+        var output = out_data.content.text//.replace(/\n/g, "<br>")
+        // alert(output)
+        requirejs(['mdui'], function(mdui) {
+            mdui.alert(output)
+        })
+    }
+    if_login_btn_show()
+}
+
+function logout() {
+    logout_code = "import os\nos.remove(os.path.join('mgr', 'auth.json'))\nprint('退出成功！')"
+    var callback = {
+        iopub: {
+            output: notice,
+        }
+    }
+    IPython.notebook.kernel.execute(logout_code, callback)
+}
+
+function login() {
+    username = $("#username").val()
+    pwd = $("#pwd").val()
+    $("#username").val("")
+    $("#pwd").val("")
+    login_code = "import mgr\nmgr.login('" + username + "', '" + pwd + "')"
+    var callback = {
+        iopub: {
+            output: notice,
+        }
+    }
+    IPython.notebook.kernel.execute(login_code, callback)
+}
+
+function sub() {
+    var code = get_source_code()
+    // alert(typeof(code))
+    var code = code.replace(/\"\"\"/g, "\\\"\\\"\\\"")
+    var filename = get_filename()
+    var sub_code = "import mgr\nmgr.substr(\"\"\"" + filename + "\"\"\", \"\"\"" + code + "\"\"\")"
+    IPython.notebook.save_notebook()
+    var callback = {
+        iopub: {
+            output: output_,
+        }
+    }
+    IPython.notebook.kernel.execute(sub_code, callback)
+}
+
+function run_cell() {
+    var cell_i = 0
+    var divs = document.getElementsByTagName("div")
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].getAttribute("class") && (divs[i].getAttribute("class").indexOf("cell text_cell") != -1 || divs[i].getAttribute("class").indexOf("cell code_cell") != -1)) {
+            if (divs[i].getAttribute("class").indexOf("cell code_cell") != -1 && divs[i].textContent.indexOf(FLAG) != -1) {
+                Jupyter.notebook.execute_cells([cell_i - 1])
+                return true
+            }
+            cell_i++
+        }
+    }
+    return false
+}
+
+IPython.notebook.save_notebook()
