7b98febb641e6cff08ba9423ab2c498d0f1baa37
RainGather <raingather@outlook.com>
2019-09-16 12:08:23 +0800

    [bugfix] install

--- start.bat
+++ start.bat
@@ -1,11 +1,12 @@
 @echo off
 
 echo 系统更新中...
+cd %~dp0
+python start.py
 cd %~dp0\tutorial
 git stash 2>NUL || C:\git\bin\git.exe stash
 git pull origin master 2>NUL || C:\git\bin\git.exe pull origin master
 git stash pop 2>NUL || C:\git\bin\git.exe stash pop
 git stash clear 2>NUL || C:\git\bin\git.exe stash clear
-python start.py
 echo 系统启动中...
 jupyter-notebook
