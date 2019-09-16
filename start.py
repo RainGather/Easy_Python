4dcd4039a081455030bc21de5c9309d8f1f22ff1
RainGather <raingather@outlook.com>
2019-09-16 12:13:04 +0800

    [bugfix] install

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 import subprocess
 
@@ -5,8 +6,18 @@ import subprocess
 def trust_notebook():
     pro_dir = pathlib.Path(__file__).parent
     tutorial_dir = pro_dir / 'tutorial'
+    trust_list_path = pro_dir / 'trust_list.txt'
+    if trust_list_path.exists():
+        with trust_list_path.open('r', encoding='utf-8') as fr:
+            trust_list = json.load(fr)['trust_list']
+    else:
+        trust_list = []
     for p in tutorial_dir.glob('**/*.ipynb'):
-        subprocess.call(f'jupyter trust {p}', shell=True)
+        if p.name not in trust_list:
+            subprocess.call(f'jupyter trust {p}', shell=True)
+            trust_list.append(p.name)
+            with trust_list_path.open('w', encoding='utf-8') as fw:
+                json.dump({'trust_list': trust_list}, fw)
 
 
 if __name__ == '__main__':
